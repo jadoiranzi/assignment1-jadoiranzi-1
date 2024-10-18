989ff8c54e02b6b674c88d491c892aa1242a0444
jadoiranzi <iranzijado100@gmail.com>
2024-10-18 15:29:42 +0200

    Update Q1Solution.py

--- Q1Solution.py
+++ Q1Solution.py
@@ -1,7 +1,26 @@
-def findRent(start,end):
- # Your code goes here 
-      return 0
+start=int(input("enter starting time: \n"))
+end=int(input("enter ending time: \n"))
 
-start=int(input("Enter start time"))
-end=int(input("Enter end time"))
-print(findRent(start,end))
+if start>end or start>24 or end>24 :
+      print("invalid input ")
+      exit(1)
+
+total_amout =0
+    # total amout
+while start<end:
+        if 0<=start<7 or 21<=start<24 :
+                total_amout=total_amout+ 500
+                
+        elif 7<=start<10 or 19<=start<21 :
+                total_amout=total_amout+ 1000
+                
+        elif 10<=start<19:
+                total_amout=total_amout+ 1500
+            
+        else:
+                pass
+
+        start=start+1
+      
+print(f"\n total amout to be paid is {total_amout} ")
+
